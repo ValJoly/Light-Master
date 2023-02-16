012fbddca1776b52e0f1d885634483d1238fef2b
ValJoly <40001886+ValJoly@users.noreply.github.com>
2023-02-16 18:33:47 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,98 +2,103 @@
 
 import os
 from twitchio.ext import commands
-from twitchio.webhook import UserFollows
 from yeelight import Bulb
 import time
+import pyttsx3
 
-bot = commands.Bot(
-    # set up the bot
-    irc_token=os.environ['TMI_TOKEN'],
-    client_id=os.environ['CLIENT_ID'],
-    nick=os.environ['BOT_NICK'],
-    prefix=os.environ['BOT_PREFIX'],
-    initial_channels=[os.environ['CHANNEL']]
-)
-newFollower = UserFollows(to_id=55422605)
-bulb = Bulb("192.168.0.15")
-deskLampe = Bulb("192.168.0.25")
+bulb = Bulb("192.168.1.21")
+
+engine = pyttsx3.init()
 
 
-@bot.event
-async def event_ready():
-    'Called once when the bot goes online.'
-    print(f"{os.environ['BOT_NICK']} is online!")
-    ws = bot._ws  # this is only needed to send messages within event_ready
-    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
+class Bot(commands.Bot):
+    
+    def __init__(self):
+        super().__init__(token=os.environ['ACCESS_TOKEN'], prefix=os.environ['BOT_PREFIX'], initial_channels=[os.environ['CHANNEL']])
 
-@bot.command(name='pink')
-async def pink(ctx):
-    bulb.turn_on()
-    bulb.set_brightness(100)
-    bulb.set_rgb(255, 153, 204)
-    print(type(ctx))
+    async def event_ready(self):
+        'Called once when the bot goes online.'
+        print(f"{os.environ['BOT_NICK']} is online!")
 
-@bot.command(name='white')
-async def white(ctx):
-    bulb.turn_on()
-    bulb.set_brightness(100)
-    bulb.set_color_temp(3500)
 
-@bot.command(name='orange')
-async def orange(ctx):
-    bulb.turn_on()
-    bulb.set_brightness(100)
-    bulb.set_rgb(240, 99, 0)
+    @commands.command(name='pink')
+    async def pink(self, ctx: commands.Context):
+        bulb.turn_on()
+        bulb.set_brightness(100)
+        bulb.set_rgb(255, 153, 204)
+        print(type(ctx))
 
-@bot.command(name='green')
-async def green(ctx):
-    bulb.turn_on()
-    bulb.set_brightness(100)
-    bulb.set_rgb(0, 255, 0)
+    @commands.command(name='white')
+    async def white(self, ctx: commands.Context):
+        bulb.turn_on()
+        bulb.set_brightness(100)
+        bulb.set_color_temp(3500)
 
-@bot.command(name='blue')
-async def blue(ctx):
-    bulb.turn_on()
-    bulb.set_brightness(100)
-    bulb.set_rgb(0, 0, 255)
+    @commands.command(name='orange')
+    async def orange(self, ctx: commands.Context):
+        bulb.turn_on()
+        bulb.set_brightness(100)
+        bulb.set_rgb(240, 99, 0)
 
-@bot.command(name='red')
-async def red(ctx):
-    bulb.turn_on()
-    bulb.set_brightness(100)
-    bulb.set_rgb(255, 0, 0)
+    @commands.command(name='green')
+    async def green(self, ctx: commands.Context):
+        bulb.turn_on()
+        bulb.set_brightness(100)
+        bulb.set_rgb(0, 255, 0)
 
-@bot.command(name='black')
-async def black(ctx):
-    # bulb.turn_off()
-    blink(bulb)
+    @commands.command(name='blue')
+    async def blue(self, ctx: commands.Context):
+        bulb.turn_on()
+        bulb.set_brightness(100)
+        bulb.set_rgb(0, 0, 255)
+
+    @commands.command(name='red')
+    async def red(self, ctx: commands.Context):
+        bulb.turn_on()
+        bulb.set_brightness(100)
+        bulb.set_rgb(255, 0, 0)
+
+    @commands.command(name='black')
+    async def black(self, ctx: commands.Context):
+        bulb.turn_off()
+        # blink(bulb)
 
 
 
-@bot.command(name='rgb')
-async def rgb(ctx, r: int, g: int, b: int):
-    if (r > 255):
-        r = 255
-    elif (r < 0):
-        r = 0
-    if (g > 255):
-        g = 255
-    elif (g < 0):
-        g = 0
-    if (b > 255):
-        b = 255
-    elif (b < 0):
-        b = 0
-    bulb.set_brightness(100)
-    bulb.set_rgb(r, g, b)
+    @commands.command(name='rgb')
+    async def rgb(self, ctx: commands.Context, r: int, g: int, b: int):
+        if (r > 255):
+            r = 255
+        elif (r < 0):
+            r = 0
+        if (g > 255):
+            g = 255
+        elif (g < 0):
+            g = 0
+        if (b > 255):
+            b = 255
+        elif (b < 0):
+            b = 0
+        bulb.set_brightness(100)
+        bulb.set_rgb(r, g, b)
 
-def blink(blb: Bulb):
-    # bulb.turn_on()
-    for i in range(0, 10):
-        blb.toggle(effect="sudden")
-        time.sleep(0.7)
-    bulb.turn_on()
+    @commands.command(name='blink')
+    async def blink(self, ctx: commands.Context):
+        # await ctx.send('blink')
+        bulb.turn_on()
+        for i in range(0, 10):
+            bulb.toggle(effect="sudden")
+            time.sleep(0.5)
+        bulb.turn_on()
+    
+    @commands.command(name='say')
+    async def say(self, ctx: commands.Context, say: str):
+        engine.say(say)
+        engine.runAndWait()
+
+
 
 if __name__ == "__main__":
+    bot = Bot()
     bot.run()
 
